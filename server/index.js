f0af033c87968f8fd1cc4c36ec7e1c2419d38dc7
The-good-guyy <80090032+The-good-guyy@users.noreply.github.com>
2024-02-25 13:24:45 +0700

    Run Server, connect local mongoDB

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("BD Connection is successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 const server = app.listen(process.env.PORT, () => {
   console.log("Server started on port 8080");
 });
